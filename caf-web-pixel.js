4a7092eca0bc48c915bd17bfc46cf601cd150003
Nguyen Hoang Gia Bao <giabao.nguyenhoang1001@gmail.com>
2024-08-22 17:01:24 +0700

    app(pixel): Access to localStorage for CAF pixel

--- caf-web-pixel.js
+++ caf-web-pixel.js
@@ -1,3 +1,9 @@
+const data = JSON.parse(localStorage.getItem("CAF_DATA_TRIGGER_EVENT"));
+const pixelID = JSON.parse(localStorage.getItem("CAF_PIXEL_ID"));
+console.log("data =====", data);
+console.log("pixelID =====", pixelID);
+const accountID = pixelID.pixelID;
+
 // Step 1. Initialize the JavaScript pixel SDK (make sure to exclude HTML)
 !(function (f, b, e, v, n, t, s) {
   if (f.fbq) return;
